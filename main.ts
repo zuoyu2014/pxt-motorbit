6edbc31c36c89ba8a732ec5b41559aeef344c420
zuoyu2014 <zuoyu2000@hotmail.com>
2022-06-19 21:34:32 +0800

    update

--- main.ts
+++ main.ts
@@ -456,39 +456,4 @@ export function MotorRunDualDelay(motor1: Motors, speed1: number, motor2: Motors
     MotorRun(motor2, 0);
     }
 
-//% blockId="motorbit_rus04" block="On-board Ultrasonic part %index show color %rgb effect %effect" 
-//% weight=78
-export function motorbit_rus04(index: RgbUltrasonics, rgb: RgbColors, effect: ColorEffect): void {
-    sensors.board_rus04_rgb(DigitalPin.P16, 4, index, rgb, effect);
-}
-    
-//% blockId=Ultrasonic_reading_distance block="On-board Ultrasonic reading distance"
-//% weight=77
-
-export function Ultrasonic_reading_distance(): number {
-    return sensors.Ultrasonic(DigitalPin.P2);
-}
-
-
-//% blockId=Setting_the_on_board_lights block="Setting the on-board lights %index color %rgb Effect %effect"
-//% weight=76
-export function Setting_the_on_board_lights(offset: Offset,rgb: RgbColors, effect: rgb_ColorEffect): void {
- sensors.board_rus04_rgb(DigitalPin.P16, offset, 0, rgb, effect);
-}
-	
-//% blockId=close_the_on_board_lights block="close the on-board lights %index color"
-//% weight=75
-export function close_the_on_board_lights(offset: Offset): void {
- sensors.board_rus04_rgb(DigitalPin.P16, offset, 0, RgbColors.Black, rgb_ColorEffect.None);
-}
-	
-//% blockId=close_all_the_on_board_lights block="close all the on-board lights"
-//% weight=74
-export function close_all_the_on_board_lights(): void {
- sensors.board_rus04_rgb(DigitalPin.P16, 0, 0, RgbColors.Black, rgb_ColorEffect.None);
- sensors.board_rus04_rgb(DigitalPin.P16, 1, 0, RgbColors.Black, rgb_ColorEffect.None);
- sensors.board_rus04_rgb(DigitalPin.P16, 2, 0, RgbColors.Black, rgb_ColorEffect.None);
- sensors.board_rus04_rgb(DigitalPin.P16, 3, 0, RgbColors.Black, rgb_ColorEffect.None);
-}
-	
 }
